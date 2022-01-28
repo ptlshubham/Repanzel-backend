25a1ddf8cd2a717063a827c7d073dffa45dbf936
Shubham Patel <ptlshubham@gmail.com>
2022-01-28 15:23:59 +0530

    Db

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "angrez"
+    database: "repanzel"
 });
 exports.executeSql = function (sql, callback) {
     con.query(sql, function (err, result) {
